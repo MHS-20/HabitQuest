8f576bb912aea3f26cdb880b226759180dcf103f
Muhamad Salman <muhamad.salman2001@gmail.com>
2026-02-21 16:25:04 +0100

    chore: updated repo name in kustomization

--- deployment/kubernetes/platform/argocd/configure.sh
+++ deployment/kubernetes/platform/argocd/configure.sh
@@ -19,8 +19,8 @@ argocd login "$argocd_server_ip" --username admin --password "$argocd_admin_pass
 
 argocd app create tracking-service \
  --repo \
- https://github.com/myhabitquest/habitquest-deployment.git \
- --path kubernetes/applications/tracking-service/ \
+ https://github.com/MHS-20/habitquest.git \
+ --path deployment/kubernetes/applications/tracking-service/ \
   --dest-server https://kubernetes.default.svc \
  --dest-namespace default \
  --sync-policy auto \
@@ -28,8 +28,8 @@ argocd app create tracking-service \
 
 argocd app create marketplace-service \
  --repo \
- https://github.com/myhabitquest/habitquest-deployment.git \
- --path kubernetes/applications/marketplace-service/ \
+ https://github.com/MHS-20/habitquest.git \
+ --path deployment/kubernetes/applications/marketplace-service/ \
   --dest-server https://kubernetes.default.svc \
  --dest-namespace default \
  --sync-policy auto \
@@ -37,8 +37,8 @@ argocd app create marketplace-service \
 
 argocd app create guild-service \
  --repo \
- https://github.com/myhabitquest/habitquest-deployment.git \
- --path kubernetes/applications/guild-service/ \
+ https://github.com/MHS-20/habitquest.git \
+ --path deployment/kubernetes/applications/guild-service/ \
   --dest-server https://kubernetes.default.svc \
  --dest-namespace default \
  --sync-policy auto \
@@ -46,8 +46,8 @@ argocd app create guild-service \
 
 argocd app create notification-service \
  --repo \
- https://github.com/myhabitquest/habitquest-deployment.git \
- --path kubernetes/applications/notification-service/ \
+ https://github.com/MHS-20/habitquest.git \
+ --path deployment/kubernetes/applications/notification-service/ \
   --dest-server https://kubernetes.default.svc \
  --dest-namespace default \
  --sync-policy auto \
@@ -55,8 +55,8 @@ argocd app create notification-service \
 
 argocd app create edge-service \
  --repo \
- https://github.com/myhabitquest/habitquest-deployment.git \
- --path kubernetes/applications/edge-service/ \
+ https://github.com/MHS-20/habitquest.git \
+ --path deployment/kubernetes/applications/edge-service/ \
   --dest-server https://kubernetes.default.svc \
  --dest-namespace default \
  --sync-policy auto \
@@ -64,8 +64,8 @@ argocd app create edge-service \
 
 argocd app create quest-service \
  --repo \
- https://github.com/myhabitquest/habitquest-deployment.git \
- --path kubernetes/applications/quest-service/ \
+ https://github.com/MHS-20/habitquest.git \
+ --path deployment/kubernetes/applications/quest-service/ \
   --dest-server https://kubernetes.default.svc \
  --dest-namespace default \
  --sync-policy auto \
