90c95677576f1249a6de348f8bdac80ebfe29350
Muhamad Salman <muhamad.salman2001@gmail.com>
2026-02-22 16:57:42 +0100

    feat: added docker file for avatar service

--- avatar-service/settings.gradle.kts
+++ avatar-service/settings.gradle.kts
@@ -1,1 +1,12 @@
 rootProject.name = "avatar-service"
+
+plugins {
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.1.9"
+}
+
+gitHooks {
+    preCommit {
+        tasks("build")
+    }
+    createHooks()
+}
