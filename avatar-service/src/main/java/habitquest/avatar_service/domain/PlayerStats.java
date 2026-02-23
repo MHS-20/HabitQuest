ce96e8ca12044c78ba9bb03860f88bb6fd4b24a5
Gabriele Basigli <gabrielebasigli@gmail.com>
2026-02-23 11:26:41 +0100

    feat: added player stats domain model

---
+++ avatar-service/src/main/java/habitquest/avatar_service/domain/PlayerStats.java
@@ -0,0 +1,11 @@
+package habitquest.avatar_service.domain;
+
+import common.ddd.Aggregate;
+
+public class PlayerStats implements Aggregate<String> {
+
+    @Override
+    public String getId() {
+        return null;
+    }
+}
