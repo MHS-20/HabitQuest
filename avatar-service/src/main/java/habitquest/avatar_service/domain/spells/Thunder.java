19be1baea981c9119e7a863da476e0e2687fab06
Muhamad Salman <muhamad.salman2001@gmail.com>
2026-02-25 10:22:31 +0100

    feat: modelled different spell types

---
+++ avatar-service/src/main/java/habitquest/avatar_service/domain/spells/Thunder.java
@@ -0,0 +1,6 @@
+package habitquest.avatar_service.domain.spells;
+
+import habitquest.avatar_service.domain.avatar.Mana;
+
+public record Thunder(String name, String description, Integer power, Mana requiredMana) implements Spell {
+}
