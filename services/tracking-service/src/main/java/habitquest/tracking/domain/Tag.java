61570b072ebdf9c06c93d2944cfd4f968714b3a4
Gabriele Basigli <gabrielebasigli@gmail.com>
2026-02-26 16:35:21 +0100

    feat: added habit domain events

--- services/tracking-service/src/main/java/habitquest/tracking/domain/Tag.java
+++ services/tracking-service/src/main/java/habitquest/tracking/domain/Tag.java
@@ -1,14 +1,13 @@
 package habitquest.tracking.domain;
 
 import common.ddd.ValueObject;
-
 import java.util.Objects;
 
 public record Tag(String name) implements ValueObject {
-    public Tag {
-        Objects.requireNonNull(name, "Tag name cannot be null");
-        if (name.isBlank()) {
-            throw new IllegalArgumentException("Tag name cannot be blank");
-        }
+  public Tag {
+    Objects.requireNonNull(name, "Tag name cannot be null");
+    if (name.isBlank()) {
+      throw new IllegalArgumentException("Tag name cannot be blank");
     }
+  }
 }
---
+++ services/tracking-service/src/main/java/habitquest/tracking/domain/events/HabitEvent.java
@@ -0,0 +1,6 @@
+package habitquest.tracking.domain.events;
+
+import common.ddd.DomainEvent;
+
+public interface HabitEvent extends DomainEvent {
+}
