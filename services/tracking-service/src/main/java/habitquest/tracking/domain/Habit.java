7f41a6f55035258162111484166e68caefa392e1
Gabriele Basigli <gabrielebasigli@gmail.com>
2026-02-26 16:14:49 +0100

    feat: added habit and recurrence domain

---
+++ services/tracking-service/src/main/java/habitquest/tracking/domain/Habit.java
@@ -0,0 +1,45 @@
+package habitquest.tracking.domain;
+
+import common.ddd.Aggregate;
+import habitquest.tracking.domain.reminder.Recurrence;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Habit implements Aggregate<String> {
+    private final String id;
+    private List<Tag> tags;
+    private String title;
+    private String description;
+    private Recurrence recurrence;
+    private LocalDate lastAttendedDate;
+
+    public Habit(String id) {
+        this.id = id;
+    }
+
+    public void attendHabit(LocalDate date) {
+        this.lastAttendedDate = date;
+    }
+    public LocalDate nextRecurrence() {
+        return this.recurrence.nextAfter(this.lastAttendedDate);
+    }
+    public LocalDate nextRecurrence(LocalDate date) {
+        return this.recurrence.nextAfter(date);
+    }
+    public List<Tag> getTags() {
+        return tags;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String getId() {
+        return this.id;
+    }
+}
