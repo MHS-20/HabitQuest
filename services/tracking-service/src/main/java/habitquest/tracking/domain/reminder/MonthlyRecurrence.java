9765714de56b463e3f908fda5a5f2c91ab20d027
Gabriele Basigli <gabrielebasigli@gmail.com>
2026-02-26 16:35:21 +0100

    feat: added habit domain events

--- services/tracking-service/src/main/java/habitquest/tracking/domain/reminder/MonthlyRecurrence.java
+++ services/tracking-service/src/main/java/habitquest/tracking/domain/reminder/MonthlyRecurrence.java
@@ -4,11 +4,9 @@ import java.time.LocalDate;
 
 public record MonthlyRecurrence(Integer dayOfMonth) implements Recurrence {
 
-    @Override
-    public LocalDate nextAfter(LocalDate current) {
-        LocalDate next = current.plusMonths(1);
-        return next.withDayOfMonth(
-                Math.min(dayOfMonth, next.lengthOfMonth())
-        );
-    }
+  @Override
+  public LocalDate nextAfter(LocalDate current) {
+    LocalDate next = current.plusMonths(1);
+    return next.withDayOfMonth(Math.min(dayOfMonth, next.lengthOfMonth()));
+  }
 }
