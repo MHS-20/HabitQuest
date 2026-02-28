77b33e9c79becaec88fba711112aaae45a2448fe
Muhamad Salman <muhamad.salman2001@gmail.com>
2026-02-28 19:02:34 +0100

    feat: added observers interfaces

---
+++ services/avatar-service/src/main/java/habitquest/avatar/application/AvatarObserverImpl.java
@@ -0,0 +1,12 @@
+package habitquest.avatar.application;
+
+import habitquest.avatar.domain.events.AvatarEvent;
+import habitquest.avatar.domain.events.AvatarObserver;
+
+public class AvatarObserverImpl implements AvatarObserver {
+
+    @Override
+    public void notifyAvaterEvent(AvatarEvent event) {
+        // TODO Auto-generated method stub
+    }
+}
