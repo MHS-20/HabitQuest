7e06012ea4bf994e27ac07b8eee0dd34b0799a13
Muhamad Salman <muhamad.salman2001@gmail.com>
2026-03-01 17:48:21 +0100

    feat: implemented guild service

--- services/guild-service/src/main/java/habitquest/guild/domain/events/GuildJoined.java
+++ services/guild-service/src/main/java/habitquest/guild/domain/events/GuildJoined.java
@@ -1,5 +1,3 @@
 package habitquest.guild.domain.events;
 
-import habitquest.guild.domain.guild.GuildMember;
-
-public record GuildJoined(String guildId, GuildMember member) implements GuildEvent {}
+public record GuildJoined(String guildId, String memberId) implements GuildEvent {}
---
+++ services/guild-service/src/main/java/habitquest/guild/domain/factory/IdGenerator.java
@@ -0,0 +1,5 @@
+package habitquest.guild.domain.factory;
+
+public interface IdGenerator {
+  String nextId();
+}
