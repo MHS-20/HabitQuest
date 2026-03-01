4e81c376239631d559362df4c8e53d04919a4764
Muhamad Salman <muhamad.salman2001@gmail.com>
2026-03-01 18:55:42 +0100

    feat: implemented guild service and updated guild domain

--- services/guild-service/src/main/java/habitquest/guild/domain/events/guildEvents/RemovedFromGuild.java
+++ services/guild-service/src/main/java/habitquest/guild/domain/events/guildEvents/RemovedFromGuild.java
@@ -1,3 +1,3 @@
-package habitquest.guild.domain.events;
+package habitquest.guild.domain.events.guildEvents;
 
 public record RemovedFromGuild(String guildId, String memberId) implements GuildEvent {}
