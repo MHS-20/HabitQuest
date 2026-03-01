4e12941e6ae374fdb8662811ae2b7190d529661f
Muhamad Salman <muhamad.salman2001@gmail.com>
2026-03-01 17:48:21 +0100

    feat: implemented guild service

--- services/guild-service/src/main/java/habitquest/guild/domain/guild/Guild.java
+++ services/guild-service/src/main/java/habitquest/guild/domain/guild/Guild.java
@@ -11,9 +11,39 @@ public class Guild implements Aggregate<String> {
   public Guild(String id, GuildMember leader) {
     this.id = id;
     this.members.add(leader);
+    this.globalRank = 0;
   }
 
   public String getId() {
     return this.id;
+  }
+
+  public List<GuildMember> getMembers() {
+    return this.members;
+  }
+
+  public Integer getGlobalRank() {
+    return this.globalRank;
+  }
+
+  public void addMember(GuildMember member) {
+    this.members.add(member);
+  }
+
+  public void removeMember(String memberId) {
+    this.members.removeIf(member -> member.getId().equals(memberId));
+  }
+
+  public void updateGlobalRank(Integer newRank) {
+    this.globalRank = newRank;
+  }
+
+  public void promoteMember(String memberId, GuildRole newRole) {
+    for (GuildMember member : members) {
+      if (member.getId().equals(memberId)) {
+        member.promoteTo(newRole);
+        break;
+      }
+    }
   }
 }
