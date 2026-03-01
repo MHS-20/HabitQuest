375ba694950ffe26b68aaa61b3ee4fc6bdc8e7c7
Gabriele Basigli <gabrielebasigli@gmail.com>
2026-03-01 15:48:37 +0100

    test: added ArchUnit clean-architecture tests for avatar

--- services/avatar-service/build.gradle.kts
+++ services/avatar-service/build.gradle.kts
@@ -18,6 +18,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-kafka-test")
     testImplementation("org.springframework.boot:spring-boot-starter-session-jdbc-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    testImplementation("com.tngtech.archunit:archunit-junit5:1.2.1")
 
     testRuntimeOnly("com.h2database:h2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
