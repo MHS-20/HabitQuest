c3f30f9c61465dc79368e8b8959734d6e78e1313
Gabriele Basigli <gabrielebasigli@gmail.com>
2026-03-04 16:23:03 +0100

    feat: added habit service event notifications

--- services/tracking-service/src/main/java/habitquest/tracking/domain/events/HabitDeleted.java
+++ services/tracking-service/src/main/java/habitquest/tracking/domain/events/HabitDeleted.java
@@ -1,5 +1,3 @@
 package habitquest.tracking.domain.events;
 
-import habitquest.tracking.domain.Habit;
-
-public record HabitDeleted(Habit habit) implements HabitEvent {}
+public record HabitDeleted(String habitId) implements HabitEvent {}
