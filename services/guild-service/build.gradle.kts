fc6f79ab89348112d3eb6a0a075769bfbee5eb00
Muhamad Salman <muhamad.salman2001@gmail.com>
2026-03-18 17:08:42 +0100

    fix: fixed notifier tests

--- services/guild-service/build.gradle.kts
+++ services/guild-service/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 description = "guild-service"
 
-val testKeycloakVersion: String by rootProject.extra
+val testArchUnit: String by rootProject.extra
 
 dependencies {
 
@@ -22,8 +22,6 @@ dependencies {
     implementation("org.springframework.retry:spring-retry")
 
     // --- Runtime ---
-    runtimeOnly("org.flywaydb:flyway-core")
-    runtimeOnly("org.flywaydb:flyway-database-postgresql")
     runtimeOnly("org.postgresql:postgresql")
     runtimeOnly("org.postgresql:r2dbc-postgresql")
     runtimeOnly("org.springframework:spring-jdbc")
@@ -31,11 +29,10 @@ dependencies {
     // --- Test ---
     testImplementation("io.r2dbc:r2dbc-h2")
     testImplementation("org.springframework.security:spring-security-test")
-    testImplementation("io.projectreactor:reactor-test")
-    testImplementation("com.github.dasniko:testcontainers-keycloak:${testKeycloakVersion}")
     testImplementation("org.testcontainers:postgresql")
     testImplementation("org.testcontainers:r2dbc")
-    testImplementation("com.tngtech.archunit:archunit-junit5:1.4.1")
-    testImplementation("org.wiremock:wiremock-standalone")
+    testImplementation("org.testcontainers:kafka")
+    testImplementation("org.springframework.kafka:spring-kafka-test")
+    testImplementation("com.tngtech.archunit:archunit-junit5:$testArchUnit")
 
 }
