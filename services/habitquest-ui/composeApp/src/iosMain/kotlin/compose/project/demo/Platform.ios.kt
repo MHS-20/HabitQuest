f5116aac35877615db22884a4e2514cbd05aa42b
Basigli <gabrielebasigli@gmail.com>
2026-03-23 23:39:57 +0100

    feat: added register page

--- services/habitquest-ui/composeApp/src/iosMain/kotlin/compose/project/demo/Platform.ios.kt
+++ services/habitquest-ui/composeApp/src/iosMain/kotlin/compose/project/demo/Platform.ios.kt
@@ -1,9 +1,15 @@
 package compose.project.demo
 
 import platform.UIKit.UIDevice
+import io.ktor.client.engine.HttpClientEngineFactory
+import io.ktor.client.engine.darwin.Darwin
 
 class IOSPlatform : Platform {
     override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
 }
 
 actual fun getPlatform(): Platform = IOSPlatform()
+
+actual fun edgeServiceBaseUrl(): String = "http://localhost:9000"
+
+actual fun createHttpEngine(): HttpClientEngineFactory<*> = Darwin
