cc119290ece1935da34bcbbdc3d9f13604402637
Muhamad Salman <muhamad.salman2001@gmail.com>
2026-03-25 11:03:45 +0100

    chore: cleaned gradle dependencies

--- services/notification-service/build.gradle.kts
+++ services/notification-service/build.gradle.kts
@@ -5,21 +5,12 @@ plugins {
 description = "notification-service"
 
 dependencies {
-    implementation("org.springframework.retry:spring-retry")
     implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.springframework.cloud:spring-cloud-stream")
-    implementation("org.springframework.cloud:spring-cloud-stream-binder-kafka")
     implementation("org.springframework.boot:spring-boot-starter-mail")
     implementation("org.springframework.boot:spring-boot-starter-json")
 
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.cloud:spring-cloud-stream-test-binder")
-    testImplementation("org.testcontainers:junit-jupiter")
-    testImplementation("org.testcontainers:kafka")
-
-    testImplementation("com.icegreen:greenmail:2.1.2")
     testImplementation("org.springframework.kafka:spring-kafka-test")
+    testImplementation("com.icegreen:greenmail:2.1.2")
     testImplementation("org.awaitility:awaitility")
 
 }
--- services/quest-service/build.gradle.kts
+++ services/quest-service/build.gradle.kts
@@ -6,20 +6,9 @@ description = "quest-service"
 val testArchUnit: String by rootProject.extra
 
 dependencies {
-
-    // --- Spring Boot ---
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.hateoas:spring-hateoas")
-    implementation("org.springframework.cloud:spring-cloud-stream")
-    implementation("org.springframework.cloud:spring-cloud-stream-binder-kafka")
 
-    // --- Utilities ---
-    implementation("org.springframework.retry:spring-retry")
-
-    // --- Test ---
-    testImplementation("org.testcontainers:kafka")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.kafka:spring-kafka-test")
     testImplementation("com.tngtech.archunit:archunit-junit5:$testArchUnit")
 }
