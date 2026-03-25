972c02c823f76776e756ff9222f1d260c5278332
Muhamad Salman <muhamad.salman2001@gmail.com>
2026-03-25 11:03:45 +0100

    chore: cleaned gradle dependencies

--- services/guild-service/build.gradle.kts
+++ services/guild-service/build.gradle.kts
@@ -7,32 +7,9 @@ description = "guild-service"
 val testArchUnit: String by rootProject.extra
 
 dependencies {
-
-    // --- Spring Boot ---
-    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
-    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.hateoas:spring-hateoas")
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.springframework.cloud:spring-cloud-stream")
-    implementation("org.springframework.cloud:spring-cloud-stream-binder-kafka")
 
-    // --- Utilities ---
-    implementation("org.springframework.retry:spring-retry")
-
-    // --- Runtime ---
-    runtimeOnly("org.postgresql:postgresql")
-    runtimeOnly("org.postgresql:r2dbc-postgresql")
-    runtimeOnly("org.springframework:spring-jdbc")
-
-    // --- Test ---
-    testImplementation("io.r2dbc:r2dbc-h2")
-    testImplementation("org.springframework.security:spring-security-test")
-    testImplementation("org.testcontainers:postgresql")
-    testImplementation("org.testcontainers:r2dbc")
-    testImplementation("org.testcontainers:kafka")
-    testImplementation("org.springframework.kafka:spring-kafka-test")
     testImplementation("com.tngtech.archunit:archunit-junit5:$testArchUnit")
-
 }
