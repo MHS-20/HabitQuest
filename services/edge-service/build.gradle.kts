1df111709470d9b3290dc8c20214b23f36bb79ab
Muhamad Salman <muhamad.salman2001@gmail.com>
2026-03-25 11:03:45 +0100

    chore: cleaned gradle dependencies

--- services/edge-service/build.gradle.kts
+++ services/edge-service/build.gradle.kts
@@ -10,19 +10,13 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-resilience4j")
     implementation("org.springframework.cloud:spring-cloud-starter-gateway-mvc")
-    implementation("org.springframework.cloud:spring-cloud-stream")
-    implementation("org.springframework.cloud:spring-cloud-stream-binder-kafka")
     implementation("io.jsonwebtoken:jjwt-api:0.12.6")
     implementation("org.springframework.boot:spring-boot-starter-json")
 
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
 
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
-    testImplementation("org.testcontainers:testcontainers")
-    testImplementation("org.testcontainers:junit-jupiter")
-    testImplementation("org.testcontainers:kafka")
     testImplementation("org.wiremock:wiremock-standalone:3.5.4")
     testImplementation("io.rest-assured:rest-assured:5.4.0")
 }
