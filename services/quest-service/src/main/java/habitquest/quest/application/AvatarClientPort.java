e1b5ff1c9168e96d8f279005f51d07b9af094b9e
Gabriele Basigli <gabrielebasigli@gmail.com>
2026-04-07 12:37:39 +0200

    feat: applying damage when quest not completed

--- services/quest-service/src/main/java/habitquest/quest/application/AvatarClientPort.java
+++ services/quest-service/src/main/java/habitquest/quest/application/AvatarClientPort.java
@@ -7,4 +7,6 @@ import habitquest.quest.domain.Avatar;
 @OutBoundPort
 public interface AvatarClientPort {
   void earnMoney(Id<Avatar> avatarId, int amount);
+
+  void applyDamage(Id<Avatar> avatarId, int amount);
 }
