2e45a11c9c0ddbca2a506a0e9096bf98d170f2f6
Muhamad Salman <muhamad.salman2001@gmail.com>
2026-04-14 19:21:52 +0200

    refactor: added common module and klint

--- habitquest-ui/composeApp/src/androidMain/kotlin/compose/project/demo/Platform.android.kt
+++ habitquest-ui/composeApp/src/androidMain/kotlin/compose/project/demo/Platform.android.kt
@@ -5,7 +5,7 @@ import io.ktor.client.engine.HttpClientEngineFactory
 import io.ktor.client.engine.okhttp.OkHttp
 
 class AndroidPlatform : Platform {
-  override val name: String = "Android ${Build.VERSION.SDK_INT}"
+    override val name: String = "Android ${Build.VERSION.SDK_INT}"
 }
 
 actual fun getPlatform(): Platform = AndroidPlatform()
